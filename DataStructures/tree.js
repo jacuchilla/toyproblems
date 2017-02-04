3a91d4f66109560833302fdf90b6c2c8fab8676b
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-02-03 16:05:54 -0800

    Updates to AddChild method for Tree

--- DataStructures/tree.js
+++ DataStructures/tree.js
@@ -20,11 +20,20 @@ var Tree = function(value) {
 
 var treeMethods = {};
 
+
+// We first need to create a method that helps us add a child to the current Tree node.
 treeMethods.addChild = function(value) {
     // First, we instantiate a new tree child
     var child = Tree(value);
     // Then, we push the new child into the current Trees children array.
     this.children.push(child);
+}
+
+// We then need to create a method that can helps us 
+treeMethods.contains = function(target) {
+    if(this.value === target) {
+        return true;
+    }
 
 }
 
