35383c781855b02dbf713ec050d1ae97c697dbd6
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-02-08 21:05:16 -0800

    Graph definition

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -5,5 +5,7 @@ ordered is called a directed graph, or just a diagraph. When pairs are ordered
 in a directed graph, an arrow is drawn from one pair to another pair. Directed 
 graphs indicate the flow direction from vertex to vertex. A path is a sequence
 of vertices in a graph such that all vertices in the path are connected by edges.
- 
+The length of a path is the number of edges from the first vertex in the path 
+to the last vertex. A path can also consist of a vertex to itself, which is 
+called a loop. 
 */
