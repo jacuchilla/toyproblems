e0be797052d8bedbbe2ce868a2e2c9644a4903ab
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-02-08 20:59:53 -0800

    Graph definition

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -1,1 +1,3 @@
-// A graph contains a set of vertices and a set of edges.
+/* A graph contains a set of vertices and a set of edges. Edges are defined 
+as a pair (v1,v2), where v1 and v2 are two vertices in a graph. 
+*/
