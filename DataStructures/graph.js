8c16ef06d6d6f04af5d0f53b452b0b97216378a2
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-02-08 21:01:13 -0800

    Graph definition

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -1,3 +1,5 @@
 /* A graph contains a set of vertices and a set of edges. Edges are defined 
-as a pair (v1,v2), where v1 and v2 are two vertices in a graph. 
+as a pair (v1,v2), where v1 and v2 are two vertices in a graph. A vertix can 
+also have a weight, which is comtimes called a cost. A graph whose pairs are 
+ordered is called a directed graph, or just a diagraph.  
 */
