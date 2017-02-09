d6fd9636558d32e523e7c94334ac1f656691bc7a
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-02-08 21:03:06 -0800

    Graph definition

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -2,5 +2,6 @@
 as a pair (v1,v2), where v1 and v2 are two vertices in a graph. A vertix can 
 also have a weight, which is comtimes called a cost. A graph whose pairs are 
 ordered is called a directed graph, or just a diagraph. When pairs are ordered
-in a directed graph, an arrow is drawn from one pair to another pair.  
+in a directed graph, an arrow is drawn from one pair to another pair. Directed 
+graphs indicate the flow direction from vertex to vertex.  
 */
